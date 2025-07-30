de9af75fb1cc82d3858c88a38bb2c3399057af0c
Shubhamsahar <kumarshubham765484@gmail.com>
2025-07-30 16:24:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -26,6 +26,18 @@ class Square extends Rectangle {
     getPerimeter() {
         return 4 * this.width;
 }
+	const rectangle = new Rectangle(5, 10);
+console.log(rectangle.width);
+console.log(rectangle.height); 
+console.log(rectangle.getArea()); 
+
+const square = new Square(7);
+console.log(square.width);
+console.log(square.height); 
+console.log(square.getArea()); 
+console.log(square.getPerimeter());
+
+
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
